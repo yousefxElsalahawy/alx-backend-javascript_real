c0b149ef72308302fcc6fa4c6a8c1fbd13b34541
You <you@gmail.com>
2024-06-30 17:41:12 +0300

    commit

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,13 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
